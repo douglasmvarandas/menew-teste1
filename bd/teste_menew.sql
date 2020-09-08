ee219c04993cded0cad397cb0f6ad40e57206442
Ruan D C da Silva <35272112+ruandsilva@users.noreply.github.com>
2020-09-08 00:01:05 -0300

    TESTE MENEW 1 PHP - V1.0
    APLICACAO AGENDA

--- bd/teste_menew.sql
+++ bd/teste_menew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Set-2020 às 18:16
+-- Tempo de geração: 08-Set-2020 às 04:58
 -- Versão do servidor: 10.4.6-MariaDB
 -- versão do PHP: 7.3.9
 
@@ -43,7 +43,7 @@ CREATE TABLE `entidade` (
 --
 
 INSERT INTO `entidade` (`idEntidade`, `nome`, `telefone`, `email`, `cidade`, `estado`, `tipo`) VALUES
-(5, 'Ruan Teste', '83994074756', 'ruandiego@mvarandas.com.br', 'asdas', 'CE', 'CLI'),
+(5, 'Ruan', '83994074756', 'ruandiego@mvarandas.com.br', 'asdas', 'RJ', 'FUN'),
 (6, 'Jason Bourne Teste', '8654213546', 'jason@menew.com', 'João Pessoa', 'PB', 'CLI'),
 (7, 'Teste', '321654987', 'teste@menew.com.br', 'Cuité', 'PB', 'FUN'),
 (8, 'Teste 2', '958492515', 'teste2@menew.com', 'Jampamar', 'RJ', 'FUN'),
@@ -57,7 +57,53 @@ INSERT INTO `entidade` (`idEntidade`, `nome`, `telefone`, `email`, `cidade`, `es
 (16, 'Jason Bourne Teste', '8654213546', 'jason@menew.com', 'João Pessoa', 'PB', 'CLI'),
 (17, 'Jason Bourne Teste', '8654213546', 'jason@menew.com', 'João Pessoa', 'PB', 'CLI'),
 (18, 'asdasdasd', '65656546', 'teste3@menew.com', 'asdasd', 'RJ', 'FUN'),
-(19, 'Ruan Diego TESTE', '321651321', 'teste3@menew.com', 'Cabedelo', 'PB', 'FUN');
+(19, 'Ruan Diego TESTE', '321651321', 'teste3@menew.com', 'Cabedelo', 'PB', 'FUN'),
+(20, 'Jfjsbfjdjfn', '62646894956', 'ruandiegocs18@gmail.com', 'Hdhdhsjcjsjd', 'SP', 'FOR'),
+(21, 'Porco pia', '9467616464', 'porcogordo@funnybunny.com', 'Oitizeiro', 'PB', 'CLI');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estados`
+--
+
+CREATE TABLE `estados` (
+  `idEstado` int(11) NOT NULL,
+  `nomeEstado` varchar(50) NOT NULL,
+  `uf` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `estados`
+--
+
+INSERT INTO `estados` (`idEstado`, `nomeEstado`, `uf`) VALUES
+(1, 'CEARÁ', 'CE'),
+(2, 'PARAÍBA', 'PB'),
+(3, 'PERNAMBUCO', 'PE'),
+(4, 'RIO DE JANEIRO', 'RJ'),
+(5, 'SÃO PAULO', 'SP');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipoentidade`
+--
+
+CREATE TABLE `tipoentidade` (
+  `idTipoEntidade` int(11) NOT NULL,
+  `nomeEntidade` varchar(255) NOT NULL,
+  `sigla` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tipoentidade`
+--
+
+INSERT INTO `tipoentidade` (`idTipoEntidade`, `nomeEntidade`, `sigla`) VALUES
+(1, 'Cliente', 'CLI'),
+(2, 'Fornecedor', 'FOR'),
+(3, 'Funcionário', 'FUN');
 
 --
 -- Índices para tabelas despejadas
@@ -70,6 +116,18 @@ ALTER TABLE `entidade`
   ADD PRIMARY KEY (`idEntidade`);
 
 --
+-- Índices para tabela `estados`
+--
+ALTER TABLE `estados`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Índices para tabela `tipoentidade`
+--
+ALTER TABLE `tipoentidade`
+  ADD PRIMARY KEY (`idTipoEntidade`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -77,7 +135,19 @@ ALTER TABLE `entidade`
 -- AUTO_INCREMENT de tabela `entidade`
 --
 ALTER TABLE `entidade`
-  MODIFY `idEntidade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `idEntidade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de tabela `estados`
+--
+ALTER TABLE `estados`
+  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `tipoentidade`
+--
+ALTER TABLE `tipoentidade`
+  MODIFY `idTipoEntidade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
