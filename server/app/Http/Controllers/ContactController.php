14e6cdfde4b54e8264949ca3c9f0d3540c30fc0f
Vinnicius S <niknicius@gmail.com>
2020-09-17 23:55:08 -0300

    feat: Contact edit

--- server/app/Http/Controllers/ContactController.php
+++ server/app/Http/Controllers/ContactController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
+use Illuminate\View\View;
 
 class ContactController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Application|Factory|View
      */
     public function index()
     {
@@ -24,7 +27,7 @@ class ContactController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Application|Factory|View
      */
     public function create()
     {
@@ -91,11 +94,15 @@ class ContactController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return mixed
      */
     public function edit($id)
     {
-        //
+        $contact = Contact::find($id);
+        $userID = Auth::id();
+
+        return $contact->user_id === $userID ? view('edit-contact', compact('contact')) : response()
+            ->json(['error' => 'Unauthenticated.'], 401);
     }
 
     /**
@@ -103,11 +110,40 @@ class ContactController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return mixed
      */
     public function update(Request $request, $id)
     {
-        //
+        $userID = Auth::id();
+
+        $types = ['Cliente', 'Fornecedor', 'Funcionário'];
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'telephone' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'category' => ['required', Rule::in($types)]
+        ]);
+
+        $contact = Contact::find($id);
+
+        if($contact === null){
+            return response()
+                ->json(['error' => 'Contact not found.'], 404);
+        }
+
+        $contact->name = $request->name;
+        $contact->telephone = $request->telephone;
+        $contact->email = $request->email;
+        $contact->city = $request->city;
+        $contact->state = $request->state;
+        $contact->category = $request->category;
+
+        $contact->save();
+
+        return redirect('contacts');
     }
 
     /**
--- server/resources/views/edit-contact.blade.php
+++ server/resources/views/edit-contact.blade.php
@@ -17,31 +17,38 @@
                             </ul>
                         </div>
                     @endif
-                    <form method='post' action="{{url('contacts')}}">
+                    <form method='post' action="{{url("contacts/$contact->id")}}">
                         @csrf
+                        @method('PUT')
                         <div class="form-group">
-                            <input class="form-control" type="text" name="name" placeholder="{{__('Name')}}" required>
+                            <input class="form-control" type="text" name="name" placeholder="{{__('Name')}}"
+                                   value="{{$contact->name}}" required>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" type="text" name="email" placeholder="{{__('Email')}}" required>
+                            <input class="form-control" type="text" name="email" placeholder="{{__('Email')}}"
+                                   value="{{$contact->email}}" required>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" type="text" name="telephone" placeholder="{{__('Telephone')}}" required>
+                            <input class="form-control" type="text" name="telephone" placeholder="{{__('Telephone')}}"
+                                   value="{{$contact->telephone}}" required>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" type="text" name="city" placeholder="{{__('City')}}" required>
+                            <input class="form-control" type="text" name="city" placeholder="{{__('City')}}"
+                                   value="{{$contact->city}}" required>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" type="text" name="state" placeholder="{{__('State')}}" required>
+                            <input class="form-control" type="text" name="state" placeholder="{{__('State')}}"
+                                   value="{{$contact->state}}" required>
                         </div>
                         <div class="form-group">
                             <select class="form-control" name="category">
-                                <option value="Cliente" selected="">Cliente</option>
+                                <option value="{{$contact->category}}" selected="">{{$contact->category}}</option>
+                                <option value="Cliente">Cliente</option>
                                 <option value="Fornecedor">Fornecedor</option>
                                 <option value="Funcionário">Funcionário</option>
                             </select>
                         </div>
-                        <button class="btn btn-dark btn-block" type="submit">{{__('Create')}}</button>
+                        <button class="btn btn-dark btn-block" type="submit">{{__('Edit')}}</button>
                     </form>
                 </div>
             </div>
