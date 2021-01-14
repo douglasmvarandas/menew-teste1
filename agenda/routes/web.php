df2161d7815df20d44fbf2b031d15b64f0d09f27
heitorvaldeger <aurelio.heitor@hotmail.com>
2021-01-14 11:07:27 -0300

    Criação do dump do BD e ajustes na rota principal

--- agenda/routes/web.php
+++ agenda/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('contato.index');
 });
 
 Route::prefix('contatos')->group(function () {
