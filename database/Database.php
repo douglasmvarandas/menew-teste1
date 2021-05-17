1b65aa5505e8e57cb8b40649a95b2990e85ee2cc
juliasc12 <50711097+juliasc12@users.noreply.github.com>
2021-05-16 21:19:38 -0300

    database files

---
+++ database/Database.php
@@ -0,0 +1,24 @@
+<?php
+    class Database{
+        private $host = "localhost";
+        private $db_name = "teste1";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+
+        public function dbConnection(){
+    	    $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password,
+                  array(
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                  ));
+    			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+    		catch(PDOException $exception){
+                echo "Connection error: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    }
