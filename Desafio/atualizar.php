2b0eb639129833ea68374e6dbffb7bd01bc4c384
Thiago Gambati de Souza <thiagogambati@outlook.com>
2021-05-19 19:55:12 -0300

    Desafio Completo

--- Desafio/atualizar.php
+++ Desafio/atualizar.php
@@ -1,7 +1,9 @@
 <?php 
 
+//chamando função para conectar no banco
 include ('connect.php');
 
+//declarando variáveis para receber dados do formulário html
 $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -10,18 +12,23 @@ $cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_STRING);
 $estado = filter_input(INPUT_POST, 'estado', FILTER_SANITIZE_STRING);
 $categoria = filter_input(INPUT_POST, 'categoria', FILTER_SANITIZE_STRING);
 
-
+//variável que guarda comando sql para editar registros
 $sql = "UPDATE agenda SET nome ='$nome', email ='$email', telefone ='$telefone', cidade = '$cidade', estado = '$estado', categoria = '$categoria' WHERE idcadastro = '$id'";
 
+//função que executa o comando no banco de dados
 mysqli_query($connect, $sql) or die($mysqli->error);;
 
+//função que verfica se foi feita alguma edição. 
 if(mysqli_affected_rows($connect) > 0){
-    echo "<script>alert('Sucesso: Atualizado corretamente!');
+    //exibindo um alerta caso tenha tido alterações e redirecionando à página inicial
+    echo "<script>alert('Sucesso: Atualizado corretamente!'); 
     window.location='index.php';</script>";
   }else{
+    //caso não tenha tido alterações é feito um aviso e redireciona a pagina inicial
     echo "<script>alert('Aviso: Não foi atualizado!);
     window.location='index.php';</script>";
   }
+  //fechando conexão com o banco
 mysqli_close($connect)
 
 ?>
--- Desafio/listar.php
+++ Desafio/listar.php
@@ -1,14 +1,15 @@
 <?php
-
+//chamando função para conectar no banco
 include('connect.php');
 
 $sql = "SELECT * FROM `agenda` ORDER BY 'idcadastro'";
 $result = mysqli_query($connect, $sql) or die($mysqli->error);
 
-//verificação
+//verificando se tem algum registro
 
 if (($result) AND ($result -> num_rows !=0)){
     
+    //loop para listar registros
     while($row_sql = mysqli_fetch_assoc($result)){    
        echo"<tr>
                 <td>". $row_sql['idcadastro'] ."</td>";
@@ -18,6 +19,8 @@ if (($result) AND ($result -> num_rows !=0)){
             echo"<td>". $row_sql['cidade'] ."</td>";
             echo"<td>". $row_sql['estado'] ."</td>";
             echo"<td>". $row_sql['categoria'] ."</td>";
+            
+            //botão para editar os registros redirecionando para o 'atualiza.php'
             echo"<td>
                     <a href ='atualiza.php?edit= {$row_sql['idcadastro']}' class = 'btn btn-info'>Editar
                     </td>";
@@ -25,8 +28,10 @@ if (($result) AND ($result -> num_rows !=0)){
 
 
 }else{
+    //exibindo aviso caso não haja registros
     echo " <h1> alert('Nenhum registro encontrado')</h1>";
 }
+//fechando conexão com o banco
 mysqli_close($connect);
 
 ?>
