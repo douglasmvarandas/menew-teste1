3e135e494d7eafbfa7f612c345da7d6024ad6d2f
Andressa Gabrielle <andressagss21@gmail.com>
2021-06-14 10:34:02 -0300

    Footer atualizado

--- cadastro.php
+++ cadastro.php
@@ -1,64 +1,67 @@
-<?php 
-    include_once 'assets/includes/header.php';
+<?php
+include_once 'assets/includes/header.php';
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col pt-3">
-            <h1>Cadastre um novo contato: </h1> <br>
-            <form action="envio-insert.php" method="POST" class="mx-auto" style="width: 70%;">
-                <div class="mb-3">
-                    <label for="nome" class="form-label">Nome:</label>
-                    <input type="name" class="form-control" id="exampleInputEmail1" placeholder="Digite seu nome:" name="nome" required>
-                </div>
-                <div class="mb-3">
-                    <label for="telefone" class="form-label">Telefone:</label>
-                    <input type="telephone" class="form-control" name="telefone" placeholder="(xx)xxxxxxxxx" name="telefone" required>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">E-mail:</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" name="email">
-                </div>
-                <div class="mb-3">
-                    <label for="cidade" class="form-label">Cidade:</label>
-                    <input type="text" class="form-control" placeholder="Digite o nome de sua cidade:" name="cidade" required>
-                </div>
-                <div class="mb-3">
-                    <label for="estado" class="form-label">Estado:</label> 
-                    <select class="form-control" name="estado" required>
-                        <option selected disabled>Escolha seu estado:</option>
-                        <option value="PB">PB</option>
-                        <option value="PE">PE</option>
-                        <option value="RN">RN</option>
-                        <option value="CE">CE</option>
-                        <option value="SE">SE</option>
-                        <option value="BA">BA</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                <label for="estado" class="form-label">Categoria:</label> 
-                    <select class="form-control" name="categoria" required>
-                        <option selected disabled>Escolha sua categoria:</option>
-                        <option value="Cliente">Cliente</option>
-                        <option value="Fornecedor">Fornecedor</option>
-                        <option value="Funcionário">Funcionário</option>
-                    </select>
-                </div>
-                
-                <div class="d-flex justify-content-between">
-                    <a href="index.php" class="btn btn-primary">Início</a>
-                    <button type="submit" class="btn btn-success">Salvar</button>
-                    
-                </div>
-                
-            </form>
 
-            
+<div id="page-content">
+    <div class="container">
+        <div class="row">
+            <div class="col pt-3">
+                <h1>Cadastre um novo contato: </h1> <br>
+                <form action="envio-insert.php" method="POST" class="mx-auto" style="width: 70%;">
+                    <div class="mb-3">
+                        <label for="nome" class="form-label">Nome:</label>
+                        <input type="name" class="form-control" id="exampleInputEmail1" placeholder="Digite seu nome:" name="nome" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="telefone" class="form-label">Telefone:</label>
+                        <input type="telephone" class="form-control" name="telefone" placeholder="(xx)xxxxxxxxx" name="telefone" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail:</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="cidade" class="form-label">Cidade:</label>
+                        <input type="text" class="form-control" placeholder="Digite o nome de sua cidade:" name="cidade" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="estado" class="form-label">Estado:</label>
+                        <select class="form-control" name="estado" required>
+                            <option selected disabled>Escolha seu estado:</option>
+                            <option value="PB">PB</option>
+                            <option value="PE">PE</option>
+                            <option value="RN">RN</option>
+                            <option value="CE">CE</option>
+                            <option value="SE">SE</option>
+                            <option value="BA">BA</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="estado" class="form-label">Categoria:</label>
+                        <select class="form-control" name="categoria" required>
+                            <option selected disabled>Escolha sua categoria:</option>
+                            <option value="Cliente">Cliente</option>
+                            <option value="Fornecedor">Fornecedor</option>
+                            <option value="Funcionário">Funcionário</option>
+                        </select>
+                    </div>
+
+                    <div class="d-flex justify-content-between">
+                        <a href="index.php" class="btn btn-primary">Início</a>
+                        <button type="submit" class="btn btn-success">Salvar</button>
+
+                    </div>
+
+                </form>
+
+
+            </div>
         </div>
     </div>
+
 </div>
-        
 
 
-<?php 
-    include_once 'assets/includes/footer.php';
+<?php
+include_once 'assets/includes/footer.php';
 ?>
--- index.php
+++ index.php
@@ -1,35 +1,32 @@
-<?php 
-    include_once 'assets/includes/header.php';
-	include 'connect-bd.php';
+<?php
+include_once 'assets/includes/header.php';
+include 'connect-bd.php';
 
 
 ?>
 
 
-<div class="container col-xxl-8 px-4 py-5">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="assets/img/index-image.svg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="80%"  loading="lazy">
-      </div>
-      <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3" style="color:#b82525">Agenda Menew</h1>
-        <p class="lead">Organize seus contatos em um único lugar.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <a href="cadastro.php"> <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Cadastre aqui</button></a>
-          <a href="agenda.php"> <button type="button" class="btn btn-outline-secondary btn-lg px-4">Acesse sua agenda</button> </a>
-        </div>
+<div class="container col-xxl-8 px-4 py-5" id="page-content">
+  <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div class="col-10 col-sm-8 col-lg-6">
+      <img src="assets/img/index-image.svg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="80%" loading="lazy">
+    </div>
+    <div class="col-lg-6">
+      <h1 class="display-5 fw-bold lh-1 mb-3" style="color:#b82525">Agenda Menew</h1>
+      <p class="lead">Organize seus contatos em um único lugar.</p>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+        <a href="cadastro.php"> <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Cadastre aqui</button></a>
+        <a href="agenda.php"> <button type="button" class="btn btn-outline-secondary btn-lg px-4">Acesse sua agenda</button> </a>
       </div>
     </div>
   </div>
+</div>
+
 
 
 
 
 
 <?php
-    include_once 'assets/includes/footer.php'
+include_once 'assets/includes/footer.php'
 ?>
-    
-
-
-
